ce23e55dbfc2d56452ef20cec5095f78869c4376
jinjianfeng <jinjianfeng@188.com>
2012-10-31 13:16:14 +0800

    增加喜欢和取消喜欢

--- js/_util/mod.js
+++ js/_util/mod.js
@@ -9,21 +9,15 @@ Youai.Mod = {
     //增加喜欢和取消喜欢
     toggleLike:function (cfg) {
 
-        var t = cfg.eventTarget,
-            method = $(t).hasClass("on") ? "deleteItemById" : "addItemById";
+        var U = Youai.Util,
+            t = cfg.eventTarget,
+            method = $(t).hasClass("on") ? "dumpAlbum" : "likeItem";
 
-
-        var url = Youai.Util.parseUrl({"method":method, "itemId":cfg.itemId, "isvCode":cfg.isvCode}, $("#J_Sid").val());
+        var url = U.parseUrl({"method":method, "itemId":cfg.itemId, "isvCode":cfg.isvCode}, $("#J_Sid").val());
 
         var success = function (response) {
             if(response.ret[0].indexOf("SUCCESS::") != -1){
                 $(t).trigger("changeUI");
-                if(method === "addItemById"){
-
-                }
-
-                $("#J_FeedList").trigger("E_AddLikeTips");
-
             }
         };
 
