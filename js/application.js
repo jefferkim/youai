ec1067582a0a5b2ad3bb0394644a4668f8e7c677
Wang Haiyi <zuoshi.why@gmail.com>
2012-11-30 14:11:43 +0800

    页面加载时就测试是否支持 webp

--- js/application.js
+++ js/application.js
@@ -1,24 +1,13 @@
 /*入口文件13311*/
 
-var Youai = {
+window.Youai || (Youai = {})
 
-    init: function() {
+Youai.init = function() {
 
-      this.testWebpSupport()
+  Youai.router = new Youai.Router();
+  Backbone.history.start();
 
-      Youai.router = new Youai.Router();
-      Backbone.history.start();
+  Youai.Util.init();
 
-      Youai.Util.init();
-
-    },
-
-    testWebpSupport: function() {
-      var image = new Image()
-      image.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
-      image.onload = function()  { Youai.imageFormat = '_.webp' }
-      image.onerror = function() { Youai.imageFormat = '' }
-    }
-
-};
+}
 
