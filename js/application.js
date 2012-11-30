ea23a77dbdd3a9bf3681c6500c03e9173002ea23
Wang Haiyi <zuoshi.why@gmail.com>
2012-11-30 13:39:50 +0800

    test webp image format support

--- js/application.js
+++ js/application.js
@@ -4,11 +4,20 @@ var Youai = {
 
     init: function() {
 
+      this.testWebpSupport()
+
       Youai.router = new Youai.Router();
-      Backbone.history.start();     
+      Backbone.history.start();
 
       Youai.Util.init();
 
+    },
+
+    testWebpSupport: function() {
+      var image = new Image()
+      image.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+      image.onload = function()  { Youai.imageFormat = '_.webp' }
+      image.onerror = function() { Youai.imageFormat = '' }
     }
 
 };
